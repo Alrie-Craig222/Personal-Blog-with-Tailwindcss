e1398a79f13ff11f14564b5ac92905ccaaaa1ef2
unknown <almariemaestrado05.com>
2024-09-05 21:36:58 +0800

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      fontFamily: {
+        'custom': ['YourFontName', 'sans-serif'],
+      },
+      colors: {
+        'custom-blue': '#1e40af',
+        'custom-gray': '#f3f4f6',
+      },
+    },
+  },
+  plugins: [],
+}
+
